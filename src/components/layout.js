32f9f8a94f1e5265d3ae2568fa717f36534ef60e
Caleb Winner <winnerc2@msu.edu>
2020-10-11 14:03:24 -0400

    start switching css to inline CSS-in-JS format

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,24 @@
 import React from "react"
 import Banner from "./banner"
 import Footer from "./footer"
+import { css } from "@emotion/core"
+
+const cssData = css`
+    margin: 3rem auto;
+    max-width: 60%;
+    padding: 0 1rem;
+    background-color: red;
+`
 
 
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <div 
+        css={cssData}
+    >
       <Banner/>
 
-          {console.log(children)}
+        {console.log(children)}
         {children}
 
       <Footer/>
